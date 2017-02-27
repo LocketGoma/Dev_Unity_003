d886adaad3185403e6234b80fdf19d5f8b01e667
LocketGoma <nothappa@naver.com>
2017-02-27 15:01:56 +0900

    0227

---
+++ Unity003_PJ_Miazuima/Assets/Script/Unit_move_01.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit_move_01 : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    void Start()
+    { // 포인팅 지정
+        target = Waypoint.points[0];
+    }
+    void Update()
+    { // 포인팅 이동
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+
+    }
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoint.points.Length - 1)
+        {
+            print("waring");
+            Destroy(gameObject);
+        }
+        wavepointIndex++;
+        target = Waypoint.points[wavepointIndex];
+    }
+
+}
---
+++ Unity003_PJ_Miazuima/Assets/Script/Wavespawn.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// 문제점 : 본체가 사라지면 더이상 리스폰이 안됨
+public class Wavespawn : MonoBehaviour {
+    public Transform Prefab01; // 1번 유닛
+    public Transform Prefab02; // 2번 유닛
+
+
+    public Transform spawnPoint;
+
+
+    public float timeBetweenWaves = 5f; // 웨이브 시간 간격
+    public int resource=10;
+    public int cost = 2;
+    private float countdown = 2f;
+    private float cooldown = 5f;
+
+    private int waveLevel = 1;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            //  if (countdown <= 0f)
+            // {
+            SpawnWave();
+            //      countdown = timeBetweenWaves;
+            //  }
+            //  countdown -= Time.deltaTime;
+        }
+        if (cooldown <= 0f)
+        {
+            resource++;
+            cooldown = 10f;
+        }
+        cooldown -= Time.deltaTime;
+    }
+    void SpawnWave()
+    {
+        //Debug.Log("Prepare for Wave"); 
+        /*for (int i = 0; i < waveLevel; i++) // <- 요기가 비정상 작동중
+        {
+            while (cooldown > 0f)
+            {
+                cooldown -= Time.deltaTime;
+                Debug.Log("cooling \n");
+            }
+            SpawnEnemy();
+            cooldown = 1f;
+          }
+        waveLevel++;
+        */
+        if (resource <= 0)
+        {
+            Debug.Log("resource is empty");
+        }
+        else
+        {
+            SpawnEnemy();
+            resource -= cost;
+        }
+
+
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(Prefab01, spawnPoint.position, spawnPoint.rotation);
+        Debug.Log("spawn \n");
+    }
+
+
+}
